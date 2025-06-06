76952053b1c4501a8724db04f185ef308b245752
PranavThorat1432 <pranavthorat95@gmail.com>
2025-06-06 14:16:01 +0530

    Add Pages

---
+++ src/Pages/Shop.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero from '../MyComponents/Hero/Hero.jsx'
+// import './Shop.css'
+
+const Shop = () => {
+  return (
+    <div>
+      <Hero />
+    </div>
+  )
+}
+
+export default Shop
---
+++ src/Pages/ShopCategory.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShopCategory = () => {
+  return (
+    <div>
+      <h1>Shop by Category</h1>
+    </div>
+  )
+}
+
+export default ShopCategory
