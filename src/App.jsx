d7528567fb3c8b2464485c1c455d018ae14ad6cb
PranavThorat1432 <pranavthorat95@gmail.com>
2025-06-13 14:02:56 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,39 +8,38 @@ import Cart from './Pages/Cart.jsx'
 import Product from './Pages/Product.jsx'
 import LoginSignup from './Pages/LoginSignup.jsx'
 import ShopCategory from './Pages/ShopCategory.jsx'
-import Hero from './MyComponents/Hero/Hero.jsx'
-import Popular from './MyComponents/Popular/Popular.jsx';
-import Offers from './MyComponents/Offers/Offers.jsx'; // Uncomment if you want to use Offers component
-import NewCollections from './MyComponents/NewCollections/NewCollections.jsx'; // Uncomment if you want to use NewCollections component
-
+import Footer from './MyComponents/Footer/Footer.jsx'; // Uncomment if you want to use Footer component
+import men_banner from "./assets/banner.jpg"
+import women_banner from "./assets/women_banner.avif"
+import kids_banner from "./assets/kids_banner.jpg"
+import ShopContextProvider from './Context/ShopContext.jsx'
 
 function App() {
   
 
   return (
     <>
-      <div>
-        <BrowserRouter>
-          <Navbar />
+      <ShopContextProvider>
+        <div>
+          <BrowserRouter>
+            <Navbar />
 
-          <Routes>
-            <Route path="/login" element={<LoginSignup />} />
-            <Route path="/shop" element={<Shop />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/product" element={<Product />} />
-            <Route path=":productId" element={<Product />} />
-            <Route path="/mens" element={<ShopCategory category="mens" />} />
-            <Route path="/womens" element={<ShopCategory category="womens" />} />
-            <Route path="/kids" element={<ShopCategory category="kids" />} />
-          </Routes>
+            <Routes>
+              <Route path="/login" element={<LoginSignup />} />
+              <Route path="/" element={<Shop />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/product" element={<Product />} />
+              <Route path="/product/:productId" element={<Product />} />
+              <Route path="/mens" element={<ShopCategory banner={men_banner} category="men" />} />
+              <Route path="/womens" element={<ShopCategory banner={women_banner} category="women" />} />
+              <Route path="/kids" element={<ShopCategory banner={kids_banner} category="kid" />} />
+            </Routes>
 
-          <Hero />
-          <Popular />
-          <Offers />
-          <NewCollections />
+            <Footer />
 
-        </BrowserRouter>
-      </div>
+          </BrowserRouter>
+        </div>
+      </ShopContextProvider>
     </>
   )
 }
