fe474c7672137eb88110c78982abbce9f1cd948d
PranavThorat1432 <pranavthorat95@gmail.com>
2025-06-13 13:52:54 +0530

    Lists all Shopee

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import Hero from '../MyComponents/Hero/Hero.jsx'
-// import './Shop.css'
+import Popular from '../MyComponents/Popular/Popular.jsx'
+import Offers from '../MyComponents/Offers/Offers.jsx'
+import NewCollections from '../MyComponents/NewCollections/NewCollections.jsx'
+import Newsletter from '../MyComponents/NewsLetter/Newsletter'
+
 
 const Shop = () => {
   return (
     <div>
       <Hero />
+      <Popular />
+      <Offers />
+      <NewCollections />
+      <Newsletter/>
+      
     </div>
   )
 }
