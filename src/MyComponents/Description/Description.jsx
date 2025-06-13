9edfc15c76ab1b940d766b7970e8bab08653edea
PranavThorat1432 <pranavthorat95@gmail.com>
2025-06-13 14:00:19 +0530

    Description of Product

---
+++ src/MyComponents/Description/Description.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./Description.css"
+
+const Description = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">
+                Description
+            </div>
+
+            <div className="descriptionbox-nav-box fade">
+                Review (130)
+            </div>
+
+            
+        </div>
+
+        <div className="descriptionbox-description">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita illum libero eum culpa itaque ratione ullam non nam, consequatur veniam, recusandae quasi. Tenetur vel eligendi, sequi fugiat delectus amet ad.
+                Aspernatur repudiandae officia nihil! Iusto exercitationem ut esse placeat a? Magnam, suscipit harum? Excepturi consectetur dolorum, in ex temporibus praesentium, saepe illo necessitatibus iusto pariatur nihil sequi blanditiis maxime iste!
+                Ea aspernatur hic nostrum ad ipsum blanditiis praesentium repellendus velit commodi unde aliquid explicabo obcaecati alias recusandae possimus deleniti sed nam minus, animi quis! Eum consequuntur quasi ipsa quidem nisi!
+                Adipisci quod porro quae tempora repudiandae in doloribus voluptas ipsa, excepturi deleniti quia explicabo eaque ducimus reiciendis accusamus! Ex dolorum consequatur quasi ipsum dicta praesentium exercitationem? Voluptates quas veritatis esse?
+                Neque voluptatum eveniet perspiciatis quos quisquam accusamus veritatis illo aut? Quasi placeat repellat nulla facere asperiores ea dolorum id fugiat saepe! Recusandae numquam suscipit ex. Illo perspiciatis ducimus sed quia?</p>
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, deserunt eum error, saepe nostrum quos porro vitae quas nulla debitis provident dignissimos ipsa nesciunt maiores aspernatur dolores nemo minima aperiam!</p>
+            </div>
+    </div>
+  )
+}
+
+export default Description
