8c4a85b0f08477bab4c4025c9a4301d5b70835bd
PranavThorat1432 <pranavthorat95@gmail.com>
2025-06-13 13:53:28 +0530

    Product Page

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {ShopContext} from "../Context/ShopContext.jsx"
+import { useParams } from 'react-router-dom';
+import BredCrums from '../MyComponents/BredCrums/BredCrums.jsx';
+import ProductDisplay from '../MyComponents/ProductDisplay/ProductDisplay.jsx';
+import Description from '../MyComponents/Description/Description.jsx';
+import RelatedProducts from '../MyComponents/RelatedProducts/RelatedProducts.jsx';
+
 
 const Product = () => {
+  const {all_products} = useContext(ShopContext);
+  const {productId} = useParams();
+  const product = all_products?.find((e) => e.id === parseInt(productId));
   return (
     <div>
-      <h1>Product Details</h1>
+      <BredCrums product={product}/>
+      <ProductDisplay product={product}/>
+      <Description></Description>
+      <RelatedProducts></RelatedProducts>
     </div>
   )
 }
